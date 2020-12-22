04ea395ec896f0f4082289f703c27444854f63e1
Rob Jones <rbj325@gmail.com>
2020-12-22 00:52:48 -0500

    Updating gitignore

--- NgRxBank/Models/TransactionDTO.cs
+++ NgRxBank/Models/TransactionDTO.cs
@@ -1,14 +1,12 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 
 namespace NgRxBank.Models
 {
     public class TransactionDTO
     {
-        [Required]
+
         public Guid AccountId { get; set; }
 
-        [Required]
         public decimal Amount { get; set; }
 
     }
